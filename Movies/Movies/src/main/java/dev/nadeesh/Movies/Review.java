58320a4441dfd33ec770753400b333b84c725b14
Nadeesh Weerarathna <nadeesh-cs20024@stu.kln.ac.lk>
2024-05-21 14:53:21 +0530

    initial classes and attributes

---
+++ Movies/Movies/src/main/java/dev/nadeesh/Movies/Review.java
@@ -0,0 +1,18 @@
+package dev.nadeesh.Movies;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.bson.types.ObjectId;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Review {
+    @Id
+    private ObjectId id;
+    private String body;
+}
---
+++ Movies/Movies/src/test/java/dev/nadeesh/Movies/main/MoviesApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.nadeesh.Movies.main;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
