d69b40bd920363b7a2b830a6b91e51ec8869885c
Nadeesh Weerarathna <nadeesh-cs20024@stu.kln.ac.lk>
2024-07-18 10:15:23 +0530

    Style Carousel

--- Movies/Movies/src/main/resources/templates/MOvieClient/movie-gold-v1/src/Component/hero/Hero.js
+++ Movies/Movies/src/main/resources/templates/MOvieClient/movie-gold-v1/src/Component/hero/Hero.js
@@ -11,7 +11,7 @@ function Hero({movies}) {
                     return (
                         <Paper key={movie.imdbId}>
                             <div className='movie-card-container'>
-                              <div className='movie-card'>
+                              <div className='movie-card' style={{"--img": `url(${movie.backdrops[0]}`}}>
                                  <div className='movie-detail'>
                                      <div className='movie-poster'>
                                          <img src={movie.poster} alt={movie.title} />
